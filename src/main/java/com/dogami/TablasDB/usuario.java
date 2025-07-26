0a305fe896a324aa0d2df37da0a174190714d95b
Iván Mauricio Molina Pérez <ivancho91258@gmail.com>
2025-07-26 09:33:51 -0500

    Cambios generales

---
+++ src/main/java/com/dogami/TablasDB/usuario.java
@@ -0,0 +1,76 @@
+package com.dogami.TablasDB;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class usuario {
+
+        @Id
+        @Column(name = "id")
+        private int id;
+        @Column(name = "nombre")
+        private String nombre;
+        @Column(name = "correo")
+        private String correo;
+        @Column(name = "contraseña")
+        private String contraseña;
+        @Column(name = "tipo_de_licencia")
+        private String tipo_de_licencia;
+        
+        public usuario(int id, String nombre, String correo, String contraseña, String tipo_de_licencia) {
+            this.id = id;
+            this.nombre = nombre;
+            this.correo = correo;
+            this.contraseña = contraseña;
+            this.tipo_de_licencia = tipo_de_licencia;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+        public String getNombre() {
+            return nombre;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public String getCorreo() {
+            return correo;
+        }
+        public void setCorreo(String correo) {
+            this.correo = correo;
+        }
+        public String getContraseña() {
+            return contraseña;
+        }
+        public void setContraseña(String contraseña) {
+            this.contraseña = contraseña;
+        }
+
+        public String getTipo_de_licencia() {
+            return tipo_de_licencia;
+        }
+        public void setTipo_de_licencia(String tipo_de_licencia) {
+            this.tipo_de_licencia = tipo_de_licencia;
+        }
+        @Override
+        public String toString() {
+            return "usuario{" +
+                    "id=" + id +
+                    ", nombre='" + nombre + '\'' +
+                    ", correo='" + correo + '\'' +
+                    ", contraseña='" + contraseña + '\'' +
+                    ", tipo_de_licencia='" + tipo_de_licencia + '\'' + '}';
+        }
+}
+
+
+
---
+++ src/main/java/com/dogami/dbconnection/DBConnection.java
@@ -0,0 +1,32 @@
+package com.dogami.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/DOgami";
+    private static final String USER = "IvanMolina";
+    private static final String PASSWORD = "Maorintak1.";  
+
+    public static Connection getConnection() throws SQLException {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("PostgreSQL JDBC Driver no encontrado");
+            throw new SQLException("PostgreSQL JDBC Driver not found.", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.err.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+    }
+}
