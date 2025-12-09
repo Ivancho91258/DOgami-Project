f13f43ac03e2d04c16d3e1a0752c9713e374d84d
Iván Mauricio Molina Pérez <ivancho91258@gmail.com>
2025-12-09 01:15:33 -0500

    Cambios en la conexión a la DB

--- src/main/java/com/dogami/dbconnection/DBConnection.java
+++ src/main/java/com/dogami/dbconnection/DBConnection.java
@@ -5,19 +5,39 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/DOgami";
+
+    // --- CONFIGURACIÓN ---
+    private static final String DB_NAME = "dogami";
     private static final String USER = "IvanMolina";
     private static final String PASSWORD = "Maorintak1.";
+    
+    private static final String CLOUD_SQL_CONNECTION_NAME = "dogami-480702:us-east1:dogami-db-general";
 
     public static Connection getConnection() throws SQLException {
-
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             System.err.println("PostgreSQL JDBC Driver no encontrado");
             throw new SQLException("PostgreSQL JDBC Driver not found.", e);
         }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        String url;
+        
+        if (System.getenv("K_SERVICE") != null) {
+
+            url = String.format("jdbc:postgresql:///%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=%s&password=%s",
+                    DB_NAME, CLOUD_SQL_CONNECTION_NAME, USER, PASSWORD);
+            
+            // En modo nube, pasamos la URL completa con credenciales
+            return DriverManager.getConnection(url);
+            
+        } else {
+            // Modo local
+            url = String.format("jdbc:postgresql://localhost:5432/%s", DB_NAME);
+            
+            // En local, pasamos usuario y contraseña por separado
+            return DriverManager.getConnection(url, USER, PASSWORD);
+        }
     }
 
     public static void closeConnection(Connection connection) {
